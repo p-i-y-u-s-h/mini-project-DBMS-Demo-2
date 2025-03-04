0ca9f0a06543c226cd464ec6b0ae8d8936524998
Piyush Mahajan <mahajanpiyush005@gmail.com>
2025-03-04 22:34:24 +0530

    commit-4

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const helmet = require("helmet");
 const morgan = require("morgan");
+const userRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
 
 const app = express();
 dotenv.config();
@@ -15,14 +17,8 @@ app.use(express.json());
 app.use(helmet());
 app.use(morgan("common"));
 
-app.get("/",(req,res)=>{
-    res.send("welcome")
-});
-
-app.get("/users",(req,res)=>{
-    res.send("welcome user")
-})
-
+app.use("/api/users",userRoute);
+app.use("/api/auth",authRoute);
 
 app.listen(3000,()=>{
     console.log("BACKEND SEVER IS RUNNING");
