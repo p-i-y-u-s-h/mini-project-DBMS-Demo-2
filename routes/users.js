2ae12f0682ba33e29445ffdf287264fe81b4a6b2
Piyush Mahajan <mahajanpiyush005@gmail.com>
2025-03-05 00:31:16 +0530

    commit-8

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ const User = require("../models/user");
 const bcrypt = require("bcrypt");
 
 router.put("/:id", async (req, res) => {
-    if (req.body.userId == req.params.id || req.user?.isAdmin) {
+    if (req.body.userId == req.params.id || req.body.isAdmin) {
         if (req.body.password) {
             try {
                 const salt = await bcrypt.genSalt(10);
@@ -23,6 +23,20 @@ router.put("/:id", async (req, res) => {
         }
     } else {
         return res.status(403).json({ error: "You can update only your account" });
+    }
+});
+
+router.delete("/:id", async (req, res) => {
+    if (req.body.userId == req.params.id || req.body.isAdmin) {
+        try {
+            await User.findByIdAndDelete(req.params.id);
+
+            res.status(200).json({ message: "Account deleted successfully"});
+        } catch (err) {
+            return res.status(500).json({ error: "Error updating user", details: err.message });
+        }
+    } else {
+        return res.status(403).json({ error: "You can delete only your account" });
     }
 });
 
