82714ac0e68df9a89d126cccd39c4374a3710b5c
Piyush Mahajan <mahajanpiyush005@gmail.com>
2025-03-04 23:07:45 +0530

    commit-5

--- models/user.js
+++ models/user.js
@@ -1,4 +1,45 @@
 const mongoose = require("mongoose");
 const UserSchema = new mongoose.Schema({
-    
-});
+    username:{
+        type:String,
+        require:true,
+        min:4,
+        max:20,
+        unique:true
+    },
+    email:{
+        type:String,
+        require:true,
+        max:60,
+        unique:true
+    },
+    password:{
+        type:String,
+        require:true,
+        min:6
+    },
+    profilePicture:{
+        type:String,
+        default:""
+    },
+    coverPicture:{
+        type:String,
+        default:""
+    },
+    followers:{
+        type:Array,
+        default:[]
+    },
+    following:{
+        type:Array,
+        default:[]
+    },
+    isAdmin:{
+        type:Boolean,
+        default:false
+    }
+},
+{timestamps:true}
+);
+
+module.exports = mongoose.model("User" , UserSchema);
--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,30 @@
 const router = require("express").Router();
+const User = require("../models/user")
 
-router.get("/",(req,res)=>{
-    res.send("hey welcome to auth")
+// router.get("/register",async(req,res)=>{
+//     const user = await new User({
+//         username:"john",
+//         email:"john@gmail.com",
+//         password:"123456"
+//     })
+
+//     await user.save();
+//     res.send("oK")
+// })
+
+router.post("/register",async(req,res)=>{
+    const newUser = await new User({
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password
+    });
+
+    try{
+        const user = await newUser.save();
+        res.status(200).json(user)
+    } catch (err){
+        console.log(err);
+    }
 })
 
 module.exports = router
