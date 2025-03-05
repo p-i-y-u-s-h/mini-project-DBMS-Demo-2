9bd164ecd74f2b6bb2bf637c23fa4f9858bdedf8
Piyush Mahajan <mahajanpiyush005@gmail.com>
2025-03-05 22:52:14 +0530

    commit-11

--- routes/users.js
+++ routes/users.js
@@ -63,8 +63,8 @@ router.put("/:id/follow", async (req, res) => {
             }
 
             if (!user.followers.includes(req.body.userId)) {
-                await user.updateOne({ $push: { followers: req.body.userId } });
-                await currentUser.updateOne({ $push: { following: req.params.id } });
+                await user.updateOne({ $pull: { followers: req.body.userId } });
+                await currentUser.updateOne({ $pull: { following: req.params.id } });
 
                 res.status(200).json({ message: "User has been followed" });
             } else {
@@ -75,6 +75,32 @@ router.put("/:id/follow", async (req, res) => {
         }
     } else {
         res.status(403).json({ message: "You can't follow yourself" });
+    }
+});
+
+router.put("/:id/unfollow", async (req, res) => {
+    if (req.body.userId !== req.params.id) {
+        try {
+            const user = await User.findById(req.params.id);
+            const currentUser = await User.findById(req.body.userId);
+
+            if (!user || !currentUser) {
+                return res.status(404).json({ message: "User not found" });
+            }
+
+            if (user.followers.includes(req.body.userId)) {
+                await user.updateOne({ $pull: { followers: req.body.userId } });
+                await currentUser.updateOne({ $pull: { following: req.params.id } });
+
+                res.status(200).json({ message: "User has been unfollowed" });
+            } else {
+                res.status(403).json({ message: "You dont follow this user" });
+            }
+        } catch (err) {
+            res.status(500).json({ message: "Internal server error", error: err.message });
+        }
+    } else {
+        res.status(403).json({ message: "You can't unfollow yourself" });
     }
 });
 
